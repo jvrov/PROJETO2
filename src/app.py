518b40ec2ddd61259b1900c6eeb2846b3b5096e0
jvrov <111283496+jvrov@users.noreply.github.com>
2024-11-11 09:06:01 -0300

    apresentação

--- src/app.py
+++ src/app.py
@@ -29,7 +29,7 @@ from src.controllers.controller import (
 app = Flask(__name__)
 app.secret_key = '1234'
 
-# Definição das rotas e controladores
+# rotas
 app.add_url_rule('/', view_func=OlaController.as_view('ola_controller'))
 app.add_url_rule('/register', view_func=RegisterController.as_view('register'))
 app.add_url_rule('/login', view_func=LoginController.as_view('login'))
@@ -37,50 +37,22 @@ app.add_url_rule('/confirmation', view_func=ConfirmationController.as_view('conf
 app.add_url_rule('/confirmationlogin', view_func=LoginConfirmationController.as_view('confirmationlogin'))
 app.add_url_rule('/home', view_func=HomeController.as_view('home'))
 app.add_url_rule('/logout', view_func=LogoutController.as_view('logout'))
-
-# Rota para criar eventos, que será aprovada por um moderador
 app.add_url_rule('/create_bet', view_func=CreateBetController.as_view('create_bet'))
-
-# Rota para listar eventos, apenas eventos aprovados
 app.add_url_rule('/listar_eventos', view_func=ListEventsController.as_view('listar_eventos'))
-
-# Rota para listar apostas
 app.add_url_rule('/listar_bets', view_func=ListBetsController.as_view('listar_bets'))
-
-# Rota para gerenciar a carteira
 app.add_url_rule('/deposito/<int:user_id>', view_func=DepositController.as_view('deposito'))
 app.add_url_rule('/realizar_deposito/<int:user_id>', view_func=WalletController.as_view('realizar_deposito'), methods=['POST'])
 app.add_url_rule('/sacar/<int:user_id>', view_func=WithdrawController.as_view('sacar'))
-
-# Rota para participar de um evento
 app.add_url_rule('/participate/<int:event_id>', view_func=ParticipateController.as_view('participate_event'))
-
-# Rota para visualizar eventos do usuário
 app.add_url_rule('/meus_eventos', view_func=MeusEventosController.as_view('meus_eventos'))
-
-# Rota para gerenciar a wallet do usuário
 app.add_url_rule('/wallet/<int:user_id>', view_func=WalletController.as_view('wallet'))
-
-# Rotas para jogos
 app.add_url_rule('/jogo', view_func=JogoController.as_view('jogo'))
 app.add_url_rule('/jogo_cor', view_func=JogoCorController.as_view('jogo_cor'))
-
-# Rota do painel do administrador
 app.add_url_rule('/admin_dashboard', view_func=AdminDashboardController.as_view('admin_dashboard'))
-
-# Rota para eventos que estão ocorrendo agora
 app.add_url_rule('/eventosagora', view_func=EventosAgoraController.as_view('eventosagora'))
-
-# Rota para excluir eventos
 app.add_url_rule('/delete_event/<int:event_id>', view_func=DeleteEventController.as_view('delete_event'))
-
-# Rota para listar usuários
 app.add_url_rule('/listar_usuarios', view_func=ListarUsuariosController.as_view('listar_usuarios'))
-
-# Rota para perfil do usuário
 app.add_url_rule('/profile', view_func=UserProfileController.as_view('user_profile'))
-
-# Rotas para o painel do moderador e ações de aprovação
 app.add_url_rule('/moderator/dashboard', view_func=ModeratorDashboardController.as_view('moderator_dashboard'))
 app.add_url_rule('/moderator_action/<int:participacao_id>/<action>', view_func=ModeratorDashboardController.as_view('moderator_dashboard_action'))
 app.add_url_rule('/moderator/approve_event/<int:event_id>', view_func=EventApprovalController.as_view('approve_event'), methods=['POST'])  # Controller para aprovação de eventos
